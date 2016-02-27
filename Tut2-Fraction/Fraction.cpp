40f17190bda8af918ab41145952cd7f4d3a43628
Ruvania <214521716@stu.ukzn.ac.za>
2016-02-27 17:29:02 +0200

    creating function to print outputs

--- Tut2-Fraction/Fraction.cpp
+++ Tut2-Fraction/Fraction.cpp
@@ -22,11 +22,39 @@ void Fraction::fraction()
 	cin >> denom;
 }
 
+void print(Fraction x)
+{
+	int N, D;
+	int W, z;
+
+	N = x.num;
+	D = x.denom;
+
+	if (N > D)
+	{
+
+		W = N / D;
+		z = W*D;
+		N = N - z;
+
+		cout << W << " " << N << "/" << D;
+	}
+	else cout << N << "/" << D << endl;
+}
+
 int main()
 {
+	Fraction fractionObject1;
+	Fraction fractionObject2;
 	Fraction addObject;
+	Fraction printObject;
 
-	return 0;
+	cout << "Fraction 1:" << endl;
+	fractionObject1.fraction();
+	cout << "Fraction 2:" << endl;
+	fractionObject2.fraction();
+
+    return 0;
 }
 
 
