ff7a80bb682b5f8478ce31e389d02e7e3fa58532
Ruvania <214521716@stu.ukzn.ac.za>
2016-02-27 18:35:34 +0200

    creating divide function

--- Tut2-Fraction/Fraction.cpp
+++ Tut2-Fraction/Fraction.cpp
@@ -8,16 +8,19 @@ public:
 	void add(Fraction, Fraction);
 	void subtract(Fraction, Fraction);
 	void multiply(Fraction, Fraction);
+	void divide(Fraction, Fraction);
 	void fraction();
 	void print(Fraction x);
 };
 
 void Fraction::fraction()
 {
-	cout << "Please enter a numerator: ";
+	cout << "Please enter a numerator:";
 	cin >> num;
-	cout << "Please enter a denomonator: ";
+	cout << "\n" << endl;
+	cout << "Please enter a denomonator:";
 	cin >> denom;
+	cout << "\n" << endl;
 }
 
 void Fraction::add(Fraction x, Fraction y)
@@ -38,6 +41,12 @@ void Fraction::multiply(Fraction x, Fraction y)
 	denom = (x.denom*y.denom);
 }
 
+void Fraction::divide(Fraction x, Fraction y)
+{
+	num = (x.num*y.denom);
+	denom = (y.num*x.denom);
+}
+
 void print(Fraction x)
 {
 	int N, D;
@@ -53,9 +62,9 @@ void print(Fraction x)
 		z = W*D;
 		N = N - z;
 
-		cout << W << " " << N << "/" << D;
+		cout << W << " " << N << "/" << D << "\n" << endl;
 	}
-	else cout << N << "/" << D << endl;
+	else cout << N << "/" << D <<"\n"<< endl;
 }
 
 int main()
@@ -66,15 +75,19 @@ int main()
 	Fraction printObject;
 	Fraction subtractObject;
 	Fraction multiplyObject;
+	Fraction divideObject;
 
-	cout << "Fraction 1:" << endl;
+	cout << "Fraction 1:\n"<< endl;
 	fractionObject1.fraction();
-	cout << "Fraction 2:" << endl;
+	cout << "Fraction 2:\n" << endl;
 	fractionObject2.fraction();
 
 	addObject.add(fractionObject1, fractionObject2);
 	subtractObject.subtract(fractionObject1, fractionObject2);
 	multiplyObject.multiply(fractionObject1, fractionObject2);
+	divideObject.divide(fractionObject1, fractionObject2);
+
+	cout << "*****************\n" << endl;
 
 	cout << "Addition:" << endl;
 	print(addObject);
@@ -82,6 +95,8 @@ int main()
 	print(subtractObject);
 	cout << "Multiply:" << endl;
 	print(multiplyObject);
+	cout << "Divide:" << endl;
+	print(divideObject);
 
     return 0;
 }
